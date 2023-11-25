b94fca157415551b7b218ae1dbf9d356a566d83f
Rob Garbanati <rgarbana@ford.com>
2023-11-24 19:26:47 -0800

    slightly more text in folds

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 package.path = package.path .. ";/Users/rgarbana/.config/nvim/?.lua"
 require("plugins")
 vim.g.mapleader = " "
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+-- vim.wo.foldmethod = 'expr'
+-- vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.wo.foldnestmax = 1
-
+vim.opt.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').' ... '.trim(getline(v:foldend))]]
+-- vim.cmd [[highlight Folded ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE]]
