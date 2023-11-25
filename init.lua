eb149dd02b247251940c320c88ece8f5c1593ee6
Rob Garbanati <rgarbana@ford.com>
2023-11-24 18:56:53 -0800

    treesitter folds, only 1 nest

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 package.path = package.path .. ";/Users/rgarbana/.config/nvim/?.lua"
 require("plugins")
 vim.g.mapleader = " "
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.wo.foldnestmax = 1
 
