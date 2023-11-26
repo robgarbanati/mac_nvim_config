55a0995e49ee4b6f88cd7e87b824d357e5ac5030
Rob Garbanati <rgarbana@ford.com>
2023-11-25 19:35:03 -0800

    i think we can filter for c files now

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,18 +1,10 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-f>', builtin.git_files, {})
-vim.keymap.set('n', '<C-F>', builtin.find_files, {})
-vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
--- vim.keymap.set('n', '<leader>gc', builtin.live_grep{vimgrep_arguments = {'rg', '--with-filename', '--line-number', '--column', '--smart-case', '-tc'}})
--- vim.api.nvim_buf_set_keymap(0, 'n', '<C-G', "lua require'telescope.builtin'.live_grep{ vimgrep_arguments = { 'rg', '--with-filename', '--line-number', '--column', '--smart-case', '-tc' } }", {noremap=true, silent=true})
--- vim.keymap.set('n', '<leader>fc', function() builtin.live_grep({'rg', '--with-filename', '--line-number', '--column', '--smart-case', '-tc'}) end, {noremap = true, silent = true})
--- vim.keymap.set('n', '<leader>fc', function() builtin.live_grep({'--with-filename', '--line-number', '--column', '--smart-case', '-tc'}) end, {noremap = true, silent = true})
--- vim.keymap.set('n', '<C-G>', function() builtin.grep_string({ search = }) end, {noremap = true, silent = true})
--- vim.keymap.set('n', '<leader>fc', builtin.live_grep, {'rg', '--with-filename', '--line-number', '--column', '--smart-case', '-tc'})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 local telescope = require("telescope")
 local lga_actions = require("telescope-live-grep-args.actions")
+-- local extensions = require("telescope.extensions")
 
 telescope.setup {
   extensions = {
@@ -38,7 +30,18 @@ vim.keymap.set("n", "<leader>gw", live_grep_args_shortcuts.grep_word_under_curso
 vim.keymap.set("n", "<leader>gp", builtin.git_files, {})
 vim.keymap.set("n", "<leader>gf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>gg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>gc", lga_actions.quote_prompt({ postfix = " -tc " }))
+vim.keymap.set("n", "<leader>gq", lga_actions.quote_prompt())
+-- vim.keymap.set("n", "<leader>gc", lga_actions.quote_prompt({ postfix = " -tc " }))
+-- vim.keymap.set("n", "<leader>gc", telescope.extensions.live_grep_args.live_grep_args({ postfix = " --type=c " }))
+-- vim.keymap.set("n", "<leader>gc", telescope.extensions.live_grep_args.live_grep_args())
+-- vim.keymap.set("n", "<leader>gc", lga_
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set("n", "<leader>ge", ":lua require('telescope').extensions.live_grep_args.live_grep_args({postfix = ' --type=c ' })<CR>")
+vim.keymap.set("n", "<leader>gr", ':lua require("telescope").extensions.live_grep_args.live_grep_args({vimgrep_arguments = { "rg", "--with-filename", "--line-number", "--column", "--smart-case" } })<CR>')
+vim.keymap.set("n", "<leader>gc", ':lua require("telescope").extensions.live_grep_args.live_grep_args({vimgrep_arguments = { "rg", "--with-filename", "--line-number", "--column", "--smart-case", "--type=c" } })<CR>')
+vim.keymap.set("n", "<leader>gl", ':lua require("telescope").extensions.live_grep_args.live_grep_args({vimgrep_arguments = { "rg", "--with-filename", "--line-number", "--column", "--smart-case", "--type=lua" } })<CR>')
+-- vim.api.nvim_buf_set_keymap(0, 'n', '<C-G', "lua require'telescope.builtin'.live_grep{ vimgrep_arguments = { 'rg', '--with-filename', '--line-number', '--column', '--smart-case', '-tc' } }", {noremap=true, silent=true}
+vim.keymap.set("n", "<leader>ga", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>gt', "lua require'telescope.builtin'.live_grep{ vimgrep_arguments = { 'rg', '--with-filename', '--line-number', '--column', '--smart-case' } }", {noremap=true, silent=true})
 require('telescope').load_extension('fzf')
 
